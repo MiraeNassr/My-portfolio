d6e8fcc75d2199c005e4256f51fea734308afd28
MiraeNassr <miraenassr@gmail.com>
2025-03-24 01:26:47 +0100

    Add coments and clean CSS code and JS

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,23 @@
-let scrollButton = document.getElementById("scroll");
-scrollButton.innerHTML = '↑';
-scrollButton.onclick = function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-};
-
+// Script the menu to click it
 document.getElementById("menu-toggle").addEventListener("click", function() {
     document.getElementById("nav-list").classList.toggle("show");
 });
 
+
+// Script the project  when the mous over or out.
+const projectItems = document.querySelectorAll('.project-item');
+
+projectItems.forEach(item => {
+    item.addEventListener('mouseover', function(){
+        this.style.transform = 'scale(1.05)';
+        this.style.transition = 'transform 0.3s ease';
+    });
+    item.addEventListener('mouseout', function(){
+        this.style.transform = 'scale(1)';
+    });
+});
+
+//Script the Contact form, sabmit button and add alert if there is a mistake .
 document.addEventListener("DOMContentLoaded", function() {
     const contactForm = document.getElementById("contact-form");
 
@@ -48,17 +58,9 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-
-
-
-const projectItems = document.querySelectorAll('.project-item');
-
-projectItems.forEach(item => {
-    item.addEventListener('mouseover', function(){
-        this.style.transform = 'scale(1.05)';
-        this.style.transition = 'transform 0.3s ease';
-    });
-    item.addEventListener('mouseout', function(){
-        this.style.transform = 'scale(1)';
-    });
-});
+//Script the scroll button 
+let scrollButton = document.getElementById("scroll");
+scrollButton.innerHTML = '↑';
+scrollButton.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
