6d22d1248e96492c6fa2e6c27595a93b88313c30
MiraeNassr <miraenassr@gmail.com>
2025-03-24 01:03:00 +0100

    all the HTML files in one index.html and clean HTML codes

--- JS/script.js
+++ JS/script.js
@@ -48,15 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-const navLinks = document.querySelectorAll('nav a');
-navLinks.forEach(link => {
-    link.addEventListener('mouseover', function() {
-        this.style.textDecoration = 'underline';
-    });
-    link.addEventListener('mouseout', function() {
-        this.style.textDecoration = 'none';
-    });
-});
+
 
 
 const projectItems = document.querySelectorAll('.project-item');
